970da40511553183b8c1a6b32aa836d857852196
eloise <40710171@live.napier.ac.uk>
2025-09-30 17:29:46 +0100

    mardi 30

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -7,5 +7,8 @@ public class App
         System.out.println("Boo yah!");
         System.out.println("boo yahha ayah");
         System.out.println("just after changing to java 17 from 25");
+        System.out.println("Just checking on mardi");
+
+
     }
 }
