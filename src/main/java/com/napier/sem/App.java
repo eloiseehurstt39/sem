bd121c458a9bf9b9ed2bc9e06eb7d5974a5ade77
eloise <40710171@live.napier.ac.uk>
2025-09-30 17:31:01 +0100

    tuesday 30

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -7,7 +7,7 @@ public class App
         System.out.println("Boo yah!");
         System.out.println("boo yahha ayah");
         System.out.println("just after changing to java 17 from 25");
-        System.out.println("Just checking on mardi");
+        System.out.println("Just checking on tuesday");
 
 
     }
