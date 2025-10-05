326dcc3bffd10158cee2979671f9992c8acfab5c
eloise <40710171@live.napier.ac.uk>
2025-10-05 16:07:06 +0100

    ?

---
+++ src/main/java/com/napier/sem/App.java
@@ -0,0 +1,14 @@
+package com.napier.sem;
+
+public class App
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Boo yah!");
+        System.out.println("boo yahha ayah");
+        System.out.println("just after changing to java 17 from 25");
+        System.out.println("Just checking on tuesday");
+
+
+    }
+}
