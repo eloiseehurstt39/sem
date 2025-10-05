aecea96625217ca1c18c5f414e7d3138535d2339
eloise <40710171@live.napier.ac.uk>
2025-10-05 16:56:16 +0100

    changed app.java

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,5 +1,8 @@
 package com.napier.sem;
 
+import com.mongodb.client.MongoClient;   // NEW import
+import com.mongodb.client.MongoClients;  // NEW import
+
 public class App
 {
     public static void main(String[] args)
@@ -8,8 +11,11 @@ public class App
         System.out.println("boo yahha ayah");
         System.out.println("just after changing to java 17 from 25");
         System.out.println("Just checking on tuesday");
-// Connect to MongoDB
-        MongoClient mongoClient = new MongoClient("mongo-dbserver");
 
+        // Connect to MongoDB (new API)
+        MongoClient mongoClient = MongoClients.create("mongodb://mongo-dbserver:27017");
+
+        // Do something simple to test
+        System.out.println("Connected to MongoDB successfully!");
     }
 }
