e73e1e7aa52af3276548ee5bd9c147eb32990154
eloise <40710171@live.napier.ac.uk>
2025-10-16 17:34:36 +0200

    real thing was : Dockerfile for Database

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,10 +1,8 @@
 package com.napier.sem;
 
-HEAD
-
 import com.mongodb.client.MongoClient;   // NEW import
 import com.mongodb.client.MongoClients;  // NEW import
-release/0.1.0.2
+
 public class App
 {
     public static void main(String[] args)
@@ -14,14 +12,10 @@ public class App
         System.out.println("just after changing to java 17 from 25");
         System.out.println("Just checking on tuesday");
 
-<<<<<<< HEAD
-
-=======
         // Connect to MongoDB (new API)
         MongoClient mongoClient = MongoClients.create("mongodb://mongo-dbserver:27017");
 
         // Do something simple to test
         System.out.println("Connected to MongoDB successfully!");
->>>>>>> release/0.1.0.2
     }
 }
